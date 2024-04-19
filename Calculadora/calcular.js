a3cef1cb612c1ff01a291429d9d6c672229e7ac1
Mirza Arcos <166764651+Arcos-Mir@users.noreply.github.com>
2024-04-19 04:29:42 -0600

    Add files via upload

---
+++ Calculadora/calcular.js
@@ -0,0 +1,9 @@
+
+function suma() {
+    var n1 = parseFloat(document.getElementById('num1').value); 
+    var n2 = parseFloat(document.getElementById('num2').value);
+    var res = n1 + n2;
+
+    document.getElementById('resultado').value = res;
+}
+
